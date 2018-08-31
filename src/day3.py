33e47204c66898983355c5b22a946ccdbcbc248e
Nhan Cao <nhancv92@gmail.com>
2018-08-31 16:39:46 +0700

    update

--- src/day3.py
+++ src/day3.py
@@ -1,7 +1,10 @@
 # Load CSV using Pandas from URL
 from pandas import read_csv
 
-url = 'https://goo.gl/bDdBiA'
+filename = "../doc/pima-indians-diabetes.data.csv"
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-data = read_csv(url, names=names)
-print(data)
+data = read_csv(filename, names=names)
+print(data.shape)
+print(data.dtypes)
+peek = data.head(20)
+print(peek)
