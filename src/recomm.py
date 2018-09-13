35d47f738914c4994a37617664358735df592a56
Nhan Cao <nhancv92@gmail.com>
2018-09-13 11:15:02 +0700

    How to get the top-N recommendations for each user https://surprise.readthedocs.io/en/stable/FAQ.html

--- src/recomm.py
+++ src/recomm.py
@@ -1,28 +1,52 @@
-#http://surpriselib.com/
-#https://medium.com/p/d2736d2e92a8
+#https://surprise.readthedocs.io/en/stable/FAQ.html
+from collections import defaultdict
+
 from surprise import SVD
 from surprise import Dataset
-from surprise.model_selection import cross_validate
 
-# Load the movielens-100k dataset (download it if needed).
+
+def get_top_n(predictions, n=10):
+    '''Return the top-N recommendation for each user from a set of predictions.
+
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    '''
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
+
+
+# First train an SVD algorithm on the movielens dataset.
 data = Dataset.load_builtin('ml-100k')
-
-# Use the famous SVD algorithm.
-algo = SVD()
-
-# Run 5-fold cross-validation and print results.
-cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
-
-# Predicting
-# Retrieve the trainset.
 trainset = data.build_full_trainset()
+algo = SVD()
 algo.fit(trainset)
 
-# Predict a certain item
-userid = str(196)
-itemid = str(302)
-actual_rating = 4
-print(algo.predict(userid, itemid, actual_rating))
+# Than predict ratings for all pairs (u, i) that are NOT in the training set.
+testset = trainset.build_anti_testset()
+predictions = algo.test(testset)
+
+top_n = get_top_n(predictions, n=10)
+
+# Print the recommended items for each user
+for uid, user_ratings in top_n.items():
+    print(uid, [iid for (iid, _) in user_ratings])
+
 
 exit(0)
-
